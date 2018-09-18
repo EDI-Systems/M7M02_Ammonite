590b40a8a4326409eda6076cbf7834b74a23521f
EDI Systems <644580703@qq.com>
2018-09-18 14:40:08 -0400

    :fire:clean up to line up with current style

--- Project/RVMDK-STM32F767IG/copy.bat
+++ Project/RVMDK-STM32F767IG/copy.bat
@@ -1,1 +1,2 @@
-copy rvmlib_cmx_conf.h ..\..\MAmmonite\Include\Platform\CortexM\rvmlib_cmx_conf.h
+copy rvm_platform.h ..\..\MAmmonite\Include\Platform\rvm_platform.h
+copy rvm_platform_cmx_conf.h ..\..\MAmmonite\Include\Platform\CortexM\rvm_platform_cmx_conf.h
